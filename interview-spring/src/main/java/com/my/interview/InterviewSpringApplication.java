8e628b6aafeb6c43a6488c7fca2abbdbf3c7e88c
liuwei <liuwei@LAPTOP-92O37VV1>
2020-02-12 10:36:34 +0800

    初始化项目-主要学习spring源码

---
+++ interview-spring/src/main/java/com/my/interview/InterviewSpringApplication.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.my.interview;
+
+/**
+ * @author liuwei
+ *
+ */
+public class InterviewSpringApplication {
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ interview-spring/src/test/java/com/my/interview/Test.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.my.interview;
+
+/**
+ * @author liuwei
+ *
+ */
+public class Test {
+
+}
